7bafee6c535ae8a8e7c08726a0e76cbb6f7114cd
romarsh <rom4rsh@gmail.com>
2024-11-07 05:09:37 +0300

    HW 12

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,25 @@
 public class Book {
+    private String bookName;
+    private int publicationYear;
+    private  Author author;
+
+    public Book(String bookName, int publicationYear, Author author) {
+        this.bookName = bookName;
+        this.publicationYear = publicationYear;
+        this.author=author;
+
+    }
+    public String getBookName(){
+        return this.bookName;
+    }
+    public int getPublicationYear(){
+        return this.publicationYear;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+    public void setPublicationYear(int publicationYear) {
+        this.publicationYear= publicationYear;
+    }
 }
